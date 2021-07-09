9f68d3eb5f330e26efc5bac3d5dbd18f4e2e9d22
Yaiza16 <yaizavallejo16@gmail.com>
2021-07-09 09:29:49 +0200

    Fixed footer position & removed all unnecessary comments

--- dist/js/slider.js
+++ dist/js/slider.js
@@ -32,21 +32,3 @@ export default function slider(){
         },
 });
 }
-
-
-// Anisha Li
-// “Manage has supercharged our team’s workflow. The ability to maintain 
-// visibility on larger milestones at all times keeps everyone motivated.”
-
-// Ali Bravo
-// “We have been able to cancel so many other subscriptions since using 
-// Manage. There is no more cross-channel confusion and everyone is much 
-// more focused.”
-
-// Richard Watts
-// “Manage allows us to provide structure and process. It keeps us organized 
-// and focused. I can’t stop recommending them to everyone I talk to!”
-
-// Shanai Gough
-// “Their software allows us to track, manage and collaborate on our projects 
-// from anywhere. It keeps the whole team in-sync without being intrusive.”
